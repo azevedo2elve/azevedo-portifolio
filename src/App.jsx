c11a43634d0c64b97734339e0661423d911d4074
azevedo2elve <azevedogabriel00@gmail.com>
2025-04-25 11:46:20 -0300

    separando section para components e fazendo uma section de Project para separar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import Nav from './components/Nav'
 import HomeSection from './sections/HomeSection'
-import Section from './sections/Section'
-import CardProject from './components/CardProject'
+import ProjectSection from './sections/ProjectSection'
 
 // styles
 import styled from 'styled-components'
@@ -31,38 +30,7 @@ function App() {
       <Nav />
       <HomeSection />
       <StyledMain>
-        <Section id="projects" title="Projetos" alignTitle="flex-end">
-          <CardProject
-            image="https://placehold.co/150"
-            tags={['JavaScript', 'HTML', 'CSS']}
-            title="Meu Projeto"
-            description="Este é um projeto incrível que utiliza tecnologias modernas para criar uma experiência incrível para os usuários."
-          />
-          <CardProject
-            image="https://placehold.co/150"
-            tags={['PHP', 'Laravel']}
-            title="Outro Projeto"
-            description="Este projeto foi desenvolvido com PHP e Laravel para criar uma aplicação robusta e escalável."
-          />
-          <CardProject
-            image="https://placehold.co/150"
-            tags={['TypeScript', 'Laravel', 'React', 'Node', 'Express']}
-            title="Outro Projeto Incrível"
-            description="Este é um projeto incrível que utiliza tecnologias modernas para criar uma experiência incrível para os usuários."
-          />
-          <CardProject
-            image="https://placehold.co/150"
-            tags={['VueJS', 'Laravel']}
-            title="Outro Projeto Incrível"
-            description="Este é um projeto incrível que utiliza tecnologias modernas para criar uma experiência incrível para os usuários."
-          />
-          <CardProject
-            image="https://placehold.co/150"
-            tags={['Laravel', 'React']}
-            title="Outro Projeto Incrível"
-            description="Este é um projeto incrível que utiliza tecnologias modernas para criar uma experiência incrível para os usuários."
-          />
-        </Section>
+        <ProjectSection />
       </StyledMain>
     </StyledApp>
   )
